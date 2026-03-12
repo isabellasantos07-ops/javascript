8a2ec3e826e75a6a7c300f2639724c030e60711f
Niko! <isabella.santos07@escola.pr.gov.br>
2026-03-12 08:31:51 -0300

    Add files via upload

---
+++ numero.js
@@ -0,0 +1,10 @@
+const meuNumb=3
+const primeiro=1
+const segundo=2
+const operacaomatematica= primeiro+segundo
+console.log(operacaomatematica)
+
+const numeroponto=3.3
+const pontoflut=.5
+const novaop=primeiro/pontoflut
+console.log(novaop)
